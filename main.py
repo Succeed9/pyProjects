63f897f90b91f0570c8fe2deedea55fae878d24c
Succeed9 <srantarov@bk.ru>
2024-07-22 18:14:05 +0800

    Works

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask
-app = Flask(name)
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/hello/<name>')
+def hello(name):
+  return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+  return f'User {username}'
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return 'Hello, World!<br><a href="/about">About</a>'
 
-if name == 'main':
+@app.route('/about')
+def about():
+  return '<h1>DOM</h1><a href="/">Main</a>'
+
+if __name__ == '__main__':
   app.run(debug=True)
